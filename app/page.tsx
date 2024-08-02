1ab340367b0076407e0fa4aadee947e6e2398a7a
kumail-abbas <kumail@uexel.com>
2024-08-02 11:15:08 +0500

    reaction and live cursor broadcast are added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import Live from "@/components/Live";
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
+    <>
     <Room>
-      <CollaborativeApp />
-    </Room>
+      <Live/>
+      </Room>
+    </>
+      
   );
 }
